6d4e5239894190b5decc8b212ab6899ab6c8a176
Joao Vitor Venancio <joaovitorvenancio99@gmail.com>
2018-11-09 17:56:37 -0200

    Criado pacotes para melhorar a navegacao

--- Source/TrabalhoSO/src/trabalhoso/views/VerificarAcertoInimigo.java
+++ Source/TrabalhoSO/src/trabalhoso/views/VerificarAcertoInimigo.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package trabalhoso;
+package trabalhoso.views;
 
+import trabalhoso.views.TelaJogo;
 import javax.swing.SwingWorker;
 
 /**
